b229ad4dfce2687da3efd81873fa5dca533c2ceb
George Maxi <george.maxi@ucsp.edu.pe>
2026-08-19 22:31:51 -0500

    eureka server

--- eureka-server/src/test/java/com/virtualbank/discovery/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/virtualbank/discovery/EurekaServerApplicationTests.java
@@ -5,7 +5,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-    properties = "eureka.client.enabled=false")
+    properties = {
+            "eureka.client.register-with-eureka-false",
+            "eureka.client.fetch-registry=false"
+    })
 class EurekaServerApplicationTests {
   @Test
   void contextLoads() {}
